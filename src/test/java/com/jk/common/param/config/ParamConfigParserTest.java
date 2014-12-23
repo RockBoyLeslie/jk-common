899004771381fc3c4dbe5ce5a6d555379b79272e
RockBoyLeslie <leslie.li@live.com>
2014-12-23 21:01:28 +0800

    更新测试用例

--- src/test/java/com/jk/common/param/config/ParamConfigParserTest.java
+++ src/test/java/com/jk/common/param/config/ParamConfigParserTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class ParamConfigParserTest {
 
-    private static final int EXEPECTED_NODE_SIZE = 2;
+    private static final int EXEPECTED_PARAMNODE_SIZE = 2;
     
     @Test
     public void testParser() {
@@ -16,7 +16,7 @@ public class ParamConfigParserTest {
     @Test
     public void testParserNode() {
         ParamsNode node = ParamConfigParser.getConfig();
-        Assert.assertEquals(EXEPECTED_NODE_SIZE, node.size());
+        Assert.assertEquals(EXEPECTED_PARAMNODE_SIZE, node.size());
     }
     
 }
