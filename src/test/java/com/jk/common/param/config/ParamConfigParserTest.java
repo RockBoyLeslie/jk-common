1a61bb38113191d3cadbb1240747a9084f38a464
RockBoyLeslie <leslie.li@live.com>
2014-12-23 20:35:25 +0800

    新增unit test case

--- src/test/java/com/jk/common/param/config/ParamConfigParserTest.java
+++ src/test/java/com/jk/common/param/config/ParamConfigParserTest.java
@@ -1,10 +1,22 @@
 package com.jk.common.param.config;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParamConfigParserTest {
 
-    public static void main(String[] args) {
+    private static final int EXEPECTED_NODE_SIZE = 1;
+    
+    @Test
+    public void testParser() {
         ParamsNode node = ParamConfigParser.getConfig();
-        System.out.println(node.size());
+        Assert.assertNotEquals(null, node);
     }
-
+    
+    @Test
+    public void testParserNode() {
+        ParamsNode node = ParamConfigParser.getConfig();
+        Assert.assertEquals(EXEPECTED_NODE_SIZE, node.size());
+    }
+    
 }
